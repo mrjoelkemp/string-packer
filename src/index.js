5a9ce6cabf52289eddf706d489fc3e493eb9d7c8
Joel Kemp <mrjoelkemp@gmail.com>
2015-10-24 13:33:17 -0400

    Less logging

--- src/index.js
+++ src/index.js
@@ -4,15 +4,10 @@ module.exports = function({list, binSize, delimiter = ''}) {
   let packed = [];
   let copy = list.slice();
 
-  debug(`original list: ${copy.join(' ')}`);
-  debug(`list consists of ${list.length} elements`);
-  debug(`bin size is ${binSize}`);
-  debug(`delimiter is ${delimiter}`);
+  debug(`list: ${list.length} elements | bin size is ${binSize} | delimiter is ${delimiter}`);
 
   // Shortest to longest word length
   copy.sort((a, b) => a.length - b.length);
-
-  debug(`length sorted list: ${copy.join(' ')}`);
 
   if (copy[0].length > binSize) {
     throw new Error('binSize is too small to fit any items');
@@ -22,30 +17,18 @@ module.exports = function({list, binSize, delimiter = ''}) {
   let pack = '';
 
   copy.forEach((currentElement, currentElementIndex) => {
-    debug(`packing current element ${currentElement}`);
-
     pack += currentElement;
-    debug(`pack so far: ${pack}`);
 
     let next = copy[currentElementIndex + 1];
-    debug(`next element is ${next}`);
 
     let nextCanFit = next && pack.length + delimiter.length + next.length <= binSize;
 
-    debug(`can the next one fit? ${nextCanFit}`);
-
     if (!nextCanFit) {
-      debug('next one cannot fit');
       packed.push(pack);
-      debug('packed results so far: ', packed);
       pack = '';
     } else {
-      debug('adding delimiter');
       pack += delimiter;
-      debug(`packed delimiter: ${pack}`);
     }
-
-    debug(`pack so far: ${pack}`);
   });
 
   debug('packed results: ', packed);
